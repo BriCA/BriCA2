3725e07cfff87ce0070a000eeaa3d635f2f5c540
Kotone Itaya <kotone@sfc.keio.ac.jp>
2018-07-12 18:23:45 +0900

    Add WIP Python wrapper code

---
+++ src/python/component.cpp
@@ -0,0 +1,32 @@
+#include <pybind11/pybind11.h>
+#include "brica/brica.hpp"
+
+namespace py = pybind11;
+
+brica::Functor wrap_function(py::object f) {
+  return [f](brica::Dict& inputs, brica::Dict& outputs) {
+    py::dict pyinputs;
+    for (auto it = inputs.begin(); it != inputs.end(); ++it) {
+      pyinputs[py::cast(it->first)] = py::cast(it->second);
+    }
+    py::dict pyoutputs = f(pyinputs);
+  };
+}
+
+class Component : public brica::Component {
+ public:
+  Component(py::object f) : brica::Component(wrap_function(f)) {}
+};
+
+PYBIND11_MODULE(brica, m) {
+  py::class_<Component>(m, "Component")
+      .def(py::init<py::object>())
+      .def("make_in_port", &Component::make_in_port)
+      .def("make_out_port", &Component::make_out_port);
+
+  m.def("connect", &brica::connect<Component>);
+
+  py::class_<brica::VirtualTimeScheduler>(m, "VirtualTimeScheduler")
+      .def("add_component", &brica::VirtualTimeScheduler::add_component)
+      .def("step", &brica::VirtualTimeScheduler::step)
+}
