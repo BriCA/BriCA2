edba2afcf50057af35b169c99ce2beefa32541b3
ktnyt <ktnyt@users.noreply.github.com>
2018-07-19 01:53:40 +0900

    Fix preamble file name

--- include/brica/mpi.hpp
+++ include/brica/mpi.hpp
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- * brica/mpi/component.hpp
+ * brica/mpi.hpp
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
